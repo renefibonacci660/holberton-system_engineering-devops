76c6882650824072e283ac152fbdaf8366ed3068
renefibonacci660 <660@holbertonschool.com>
2019-08-05 19:54:40 +0000

    Correct formatting and creates file successfully

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Creating a file in Puppet that is owned and in group www-data
+file { '/tmp/holberton':
+  ensure  => file,
+  path    => '/tmp/holberton',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
